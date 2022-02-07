140b376e0a9221c6a152f6b03a86e88028c994e6
Lars Strömberg <stromberg.lars@gmail.com>
2022-02-07 15:30:39 +0100

    Added CORS

--- server/src/Index.ts
+++ server/src/Index.ts
@@ -1,6 +1,17 @@
 import express from 'express'
+import cors from 'cors'
+
+// Middlewares
+const allowedOrigins = ['http://localhost:3000']
+const allowedMethods = ['GET', 'POST', 'PUT', 'DELETE']
+
+const options: cors.CorsOptions = {
+	origin: allowedOrigins,
+	methods: allowedMethods
+}
 
 const app = express()
+app.use(cors(options))
 const PORT = 8000
 app.get('/', (req, res) => res.send('API is Alive with TypeScript!'))
 app.listen(PORT, () => {
